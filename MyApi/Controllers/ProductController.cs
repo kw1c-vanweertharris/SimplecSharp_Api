d711e0f81a4b0b4721d7231eb13b9191c0ca3443
harri <harri@HARRISLAPTOP>
2024-10-04 14:53:56 +0200

    Add project files.

---
+++ MyApi/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private static List<Product> products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Product 1", Price = 10.99M },
+        new Product { Id = 2, Name = "Product 2", Price = 20.99M }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        return Ok(products);
+    }
+}
---
+++ MyApi/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://127.0.0.1:3000") 
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Enable CORS
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
